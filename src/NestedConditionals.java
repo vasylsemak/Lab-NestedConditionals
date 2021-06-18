1e047c8a54d7f452dbfbe6840fe2aa8455e9cbfa
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-18 10:29:10 -0400

    practice nested statements that utilize conditional operators in Java

---
+++ src/NestedConditionals.java
@@ -0,0 +1,26 @@
+
+public class NestedConditionals {
+
+	public static void main(String[] args) {
+		   int precipitation = 0;
+		   int waterOutput = 0;
+		   int temperature = 80;
+		   
+		   if(precipitation < 20) {
+			   if(temperature <= 75) waterOutput = 30;
+			   else waterOutput = 45; 
+		   } else if(precipitation < 31) {
+			   if(temperature <= 75) waterOutput = 10;
+			   else waterOutput = 25;
+		   } else if(precipitation < 56) {
+			   if(temperature <= 75) waterOutput = 0;
+			   else waterOutput = 15;
+		   } else if(precipitation >= 56) {
+			   waterOutput = 0;
+		   }
+		
+		System.out.println("precipitation: " + precipitation + "; temperature: " + temperature + 
+				". Water output based on conditions: " + waterOutput);
+	}
+
+}
